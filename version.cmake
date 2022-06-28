4388775304698c09288086ba24e61a7db9a6494c
Gavin Halliday <gavin.halliday@lexisnexis.com>
2022-06-28 15:48:39 +0100

    Split off 7.12.112
    Signed-off-by: Gavin Halliday <gavin.halliday@lexisnexis.com>

--- version.cmake
+++ version.cmake
@@ -5,7 +5,7 @@ set ( HPCC_NAME "Nagios Monitoring" )
 set ( HPCC_PROJECT "nagios-monitoring" )
 set ( HPCC_MAJOR 7 )
 set ( HPCC_MINOR 12 )
-set ( HPCC_POINT 111 )
+set ( HPCC_POINT 113 )
 set ( HPCC_MATURITY "closedown" )
 set ( HPCC_SEQUENCE 0 )
 ###
